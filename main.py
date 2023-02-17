6376ac6ad0321e964cfcf36d5c2620ffa0606132
提交 <martian2004@qq,com>
2023-02-17 11:42:22 +0800

    Initial commit

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ import openai
 import streamlit as st
 
 # Set up OpenAI API key
-openai.api_key =  'OPENAI_API_KEY_babyname'
+if  "OPENAI_API_KEY_babyname" in locals().keys(): #判断本地是否有定义
+    openai.api_key = OPENAI_API_KEY_babyname
+else:
+    import setting                               #无则读取本地环境
+    openai.api_key = setting.get_key()
 
-def generate_names(model, parents_names, birth_date, language):
+def generate_names(model, last_names,sex):
     completions = openai.Completion.create(
         engine=model,
-        prompt=f"Generate baby names for parents with names {parents_names} born on {birth_date} in {language}",
+        prompt=f"Generate three chinese and english baby names for the last name they choice  {last_names} "
+               f"and the baby's sex {sex}",
         max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.5,
+        temperature=0.8,
     )
 
     message = completions.choices[0].text
@@ -21,12 +26,11 @@ def generate_names(model, parents_names, birth_date, language):
 # Use OpenAI's GPT-3 as the language model
 model = "text-davinci-003"
 
-st.title("Baby Name Generator")
+st.title("AI BB取名神器")
 
-parents_names = st.text_input("Enter both parents' names (e.g. John Doe & Jane Doe):")
-birth_date = st.date_input("Enter birth date:")
-language = st.selectbox("Choose language:", ["Chinese", "English"])
+last_names = st.text_input("输入你想要BB的姓氏 (e.g. 陈 & Jane Doe):")
+sex = st.selectbox("性别:", ["男", "女"])
 
 if st.button("Generate Names"):
-    names = generate_names(model, parents_names, birth_date, language)
+    names = generate_names(model, last_names,sex)
     st.success(names)
